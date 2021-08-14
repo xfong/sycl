9d0615efee549908fab202b3f037ca8d28ec5acb
Xuanyao Fong <xuanyao.fong@gmail.com>
2021-08-15 01:49:17 +0800

    clean c interface includes

--- sycl2020/library/include/create_double.sh
+++ sycl2020/library/include/create_double.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -f libmumax3cl_d.hpp ]; then
-  rm libmumax3cl_d.hpp
+if [ -f libmumax3cl_d.h ]; then
+  rm libmumax3cl_d.h
 fi
-cp libmumax3cl_f.hpp libmumax3cl_d.hpp
-sed -i 's/float/double/' libmumax3cl_d.hpp
+cp libmumax3cl_f.h libmumax3cl_d.h
+sed -i 's/float/double/' libmumax3cl_d.h
